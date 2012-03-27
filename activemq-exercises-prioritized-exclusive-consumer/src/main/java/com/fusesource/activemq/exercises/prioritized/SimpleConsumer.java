4103dbb034f0416586ed4c9ab4df3ba2973527ef
cposta <christian@fusesource.com>
2012-03-27 16:01:31 -0700

    added exercise 9, activemq-exercises-prioritized-exclusive-consumer

---
+++ activemq-exercises-prioritized-exclusive-consumer/src/main/java/com/fusesource/activemq/exercises/prioritized/SimpleConsumer.java
@@ -0,0 +1,81 @@
+package com.fusesource.activemq.exercises.prioritized;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cposta
+ * Date: 3/23/12
+ * Time: 9:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleConsumer {
+    private static final Log LOG = LogFactory.getLog(SimpleConsumer.class);
+
+    private static final Boolean NON_TRANSACTED = false;
+    private static final String CONNECTION_FACTORY_NAME = "myJmsFactory";
+    private static final String DESTINATION_NAME = "queue/simple";
+    private static final int MESSAGE_TIMEOUT_MILLISECONDS = 120000;
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            // start up an init context... properties file must be named "jndi.properties" on the root
+            // of the classpath
+            Context context = new InitialContext();
+            ConnectionFactory factory = (ConnectionFactory) context.lookup(CONNECTION_FACTORY_NAME);
+            Queue destination = (Queue) context.lookup(DESTINATION_NAME);
+
+            connection = factory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(NON_TRANSACTED, Session.AUTO_ACKNOWLEDGE);
+
+            // get the priority of the consumer from the system properties
+            String priority = System.getProperty("consumerPriority", "1");
+//            destination = session.createQueue(destination.getQueueName() + "?consumer.exclusive=true");
+            destination = session.createQueue(destination.getQueueName() + "?consumer.priority=" + priority);
+
+
+            // consumer of the queue
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            LOG.info("Start consuming messages from " + destination.toString() + " with " + MESSAGE_TIMEOUT_MILLISECONDS + "ms timeout");
+
+            // synchronous message consumer
+            int i = 1;
+            while (true) {
+                Message message = consumer.receive(MESSAGE_TIMEOUT_MILLISECONDS);
+                if (message != null) {
+                    if (message instanceof TextMessage) {
+                        String text = ((TextMessage) message).getText();
+                        LOG.info("Got " + (i++) + ", message: " + text);
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            consumer.close();
+            session.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        finally {
+            // got to clean up the connections and other resources!
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    LOG.error(e);
+                }
+            }
+        }
+    }
+}
---
+++ activemq-exercises-prioritized-exclusive-consumer/src/main/java/com/fusesource/activemq/exercises/prioritized/SimpleProducer.java
@@ -0,0 +1,74 @@
+package com.fusesource.activemq.exercises.prioritized;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cposta
+ * Date: 3/23/12
+ * Time: 9:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleProducer {
+    private static final Log LOG = LogFactory.getLog(SimpleProducer.class);
+
+    private static final Boolean NON_TRANSACTED = false;
+//    private static final long MESSAGE_TIME_TO_LIVE_MILLISECONDS = 3000;
+    private static final long MESSAGE_TIME_TO_LIVE_MILLISECONDS = 0;
+    private static final int MESSAGE_DELAY_MILLISECONDS = 100;
+    private static final int NUM_MESSAGES_TO_BE_SENT = 100;
+    private static final String CONNECTION_FACTORY_NAME = "myJmsFactory";
+    private static final String DESTINATION_NAME = "queue/simple";
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            // start up an init context... properties file must be named "jndi.properties" on the root
+            // of the classpath
+            Context context = new InitialContext();
+            ConnectionFactory factory = (ConnectionFactory) context.lookup(CONNECTION_FACTORY_NAME);
+            Destination destination = (Destination) context.lookup(DESTINATION_NAME);
+
+            connection = factory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(NON_TRANSACTED, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer producer = session.createProducer(destination);
+
+            // sets the JMSExpiration
+            producer.setTimeToLive(MESSAGE_TIME_TO_LIVE_MILLISECONDS);
+
+
+            for (int i = 1; i <= NUM_MESSAGES_TO_BE_SENT; i++) {
+                TextMessage message = session.createTextMessage(i + ". message sent");
+                LOG.info("Sending to destination: " + destination.toString() + " this text: '" + message.getText() + "'");
+                producer.send(message);
+
+                // delay the sending for a bit
+                Thread.sleep(MESSAGE_DELAY_MILLISECONDS);
+            }
+
+            producer.close();
+            session.close();
+
+        } catch (Exception e) {
+            LOG.error(e);
+        }
+        finally {
+            // got to clean up the connections and other resources!
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    LOG.error(e);
+                }
+            }
+        }
+    }
+
+}
